94e3e29851722cc50c13be246ed95ac79b15b02c
luizandre37 <lamaceno2005@gmail.com>
2022-03-20 10:21:44 -0300

    atualização de estudo de casa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-course-carsales';
+  title = 'bem vindo meu camarada';
 }
